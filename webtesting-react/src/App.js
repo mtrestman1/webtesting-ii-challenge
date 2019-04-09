1ef19703e8c9c64613b7d7000bace27356b1b4aa
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-04-09 17:29:37 -0400

    All components built out and displayed, now working on testing/ how to test within comp

--- webtesting-react/src/App.js
+++ webtesting-react/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        
+       <Dashboard />
       </div>
     );
   }
